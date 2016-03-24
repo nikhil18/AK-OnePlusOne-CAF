747ca0e5d8ec39e8014c8d93b37ffd32e4a67d44
nikhil18 <nikhilgolechha1@gmail.com>
2016-03-24 17:06:26 +0530

    Revert "disable load based sync"
    This reverts commit 540197f1c31eed5fd2150b543ce4fd404bfcb687.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -70,7 +70,7 @@ module_param(hotplug_boost, bool, 0644);
 static unsigned int migration_load_threshold = 20;
 module_param(migration_load_threshold, uint, 0644);
 
-static bool load_based_syncs;
+static bool load_based_syncs = 1;
 module_param(load_based_syncs, bool, 0644);
 
 bool wakeup_boost;
